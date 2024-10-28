d0c4b7d9b23aacd7c3479de3a26d24f7bef7ac68
Sarthak Srivastava <ss6883@srmist.edu.in>
2024-10-28 10:00:59 +0530

    Upd

--- Prime.c
+++ Prime.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
-#include <conio.h>
+
+int main(){
+    int n, d;
+
+    printf("Enter number to check prime number: ");
+    scanf("%d", &n);
+
+    if (n%2==0){
+        printf("Prime");
+    }
+    else {
+        printf("Not Prime");
+    }
+}
