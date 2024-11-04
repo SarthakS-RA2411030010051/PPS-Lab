d65e9a0b937e544aedf0925aac90154f84e2b53b
Sarthak Srivastava <ss6883@srmist.edu.in>
2024-11-04 08:25:55 +0530

    Upd

--- Elab.c
+++ Elab.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
+
 int check_armstrong(int);
 
 int main()
 {
-
     int start, end, i;
     scanf("%d %d", &start, &end);
 
-    if (start <= 0 || start >= 50 || end <= 0 || end >= 1000)
+    i = start;
+
+    if (start < 0 || start > 50 || end < 0 || end > 1000)
     {
-        printf("Invalid Output");
+        printf("Invalid Input");
     }
     else
     {
-        for (i = start; i <= end; i++)
+        while (start <= end)
         {
             check_armstrong(i);
+            i++;
+            start++;
         }
     }
-
     return 0;
 }
 
 int check_armstrong(int n)
 {
-    printf("%d ", n);
+    int originalNum, remainder, sum = 0;
+
+    originalNum = n;
+
+    while (originalNum != 0)
+    {
+
+        remainder = originalNum % 10;
+
+        sum += remainder * remainder * remainder;
+
+        originalNum /= 10;
+    }
+
+    if (n == sum)
+        printf("%d ", n);
 
     return 0;
 }
